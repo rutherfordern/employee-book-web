eecc664b6e46cef4bbff9433ff1534a7366358f8
Egor <glavnegor@yandex.ru>
2022-01-25 21:56:41 +0300

    Homework fix and done!

--- src/main/java/com/example/employeebookweb/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/employeebookweb/service/EmployeeServiceImpl.java
@@ -15,9 +15,9 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     @Override
     public boolean addEmployee(String firstName, String lastName) {
-        Employee employee1 = new Employee(firstName, lastName);
+        Employee testEmployee = new Employee(firstName, lastName);
 
-        if (employee.contains(employee1)) {
+        if (employee.contains(testEmployee)) {
             throw new EmployeeExistException();
         }
         return employee.add(new Employee(firstName, lastName));
@@ -25,10 +25,10 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     @Override
     public boolean removeEmployee(String firstName, String lastName) {
-        Employee employee1 = new Employee(firstName, lastName);
+        Employee testEmployee = new Employee(firstName, lastName);
 
-        if (employee.contains(employee1)) {
-            return employee.remove(employee1);
+        if (employee.contains(testEmployee)) {
+            return employee.remove(testEmployee);
         }
         throw new EmployeeNotFoundException();
     }
@@ -36,10 +36,10 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public Employee findEmployee(String firstName, String lastName) {
 
-        Employee employee1 = new Employee(firstName, lastName);
+        Employee testEmployee = new Employee(firstName, lastName);
 
         for (Employee valueEmployee : employee) {
-            if (valueEmployee.equals(employee1)) {
+            if (valueEmployee.equals(testEmployee)) {
                 return valueEmployee;
             }
         }
